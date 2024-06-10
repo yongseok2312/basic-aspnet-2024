488f664ca1ab3644a4567a2abc7eb530be1b45c1
yongseok2312 <lyos2312@naver.com>
2024-06-10 17:26:36 +0900

    수정 필요

---
+++ day09/WebApplication1/WebApplication1/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicWeb.Models
+{
+    /// <summary>
+    ///  DB에 테이블로 만들어지는 엔티티 클래스
+    /// </summary>
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; } // PK
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder {  get; set; }  
+        public DateTime PostDate { get; set; }  = DateTime.Now;
+
+    }
+}
